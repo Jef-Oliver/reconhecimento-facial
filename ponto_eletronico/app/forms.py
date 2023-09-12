6d110de7574e16dea76512dd56db93d39c0e5ba6
jeferson.santos <j4.oliver23@gmail.com>
2023-09-12 16:40:35 -0300

    adicionando o Django ao projeto, criando modelo de funcionário, criando views, e form para o cadastro de usuários

---
+++ ponto_eletronico/app/forms.py
@@ -0,0 +1,40 @@
+import os
+
+from django import forms
+from .models import Funcionario
+
+class FuncionarioForm(forms.ModelForm):
+
+    foto_1 = forms.ImageField(required=True)
+    foto_2 = forms.ImageField(required=True)
+    foto_3 = forms.ImageField(required=True)
+    foto_4 = forms.ImageField(required=True)
+    foto_5 = forms.ImageField(required=True)
+
+    class Meta:
+        model = Funcionario
+        fields = ('nome', 'cpf', 'rg', 'data_nascimento', 'cargo')
+
+    def save(self, commit=True):
+        funcionario = super().save(commit=False)
+
+        # Cria uma pasta para o funcionario
+        pasta_funcionario = f'fotos/funcionarios/{funcionario.id}'
+        os.makedirs(pasta_funcionario, exist_ok=True)
+
+        # Salva as fotos na pasta do funcionário
+        foto_1 = self.cleaned_data['foto_1']
+        foto_1.save(os.path.join(pasta_funcionario, 'foto_1.jpg'))
+        foto_2 = self.cleaned_data['foto_2']
+        foto_2.save(os.path.join(pasta_funcionario, 'foto_2.jpg'))
+        foto_3 = self.cleaned_data['foto_3']
+        foto_3.save(os.path.join(pasta_funcionario, 'foto_3.jpg'))
+        foto_4 = self.cleaned_data['foto_4']
+        foto_4.save(os.path.join(pasta_funcionario, 'foto_4.jpg'))
+        foto_5 = self.cleaned_data['foto_5']
+        foto_5.save(os.path.join(pasta_funcionario, 'foto_5.jpg'))
+
+        if commit:
+            funcionario.save()
+
+        return funcionario
--- ponto_eletronico/app/views.py
+++ ponto_eletronico/app/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from cv2 import threshold
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from reconhecedor.reconhecedor_fisherfaces import reconhecedor
+from .models import Funcionario
+from .forms import FuncionarioForm
+
 
 # Create your views here.
+
+def registrar_ponto(request):
+    if request.method == 'POST':
+        # Captura a imagem da face do funcionário
+        imagem = request.FILES['imagem']
+
+        # realiza o reconhecimento facial
+        identificacao, confianca = reconhecedor.predict(imagem)
+
+        # verifica se o funcionário foi reconhecido
+        if confianca > threshold:
+            # registra o ponto do funcionário
+            funcionario = Funcionario.objects.get(id=identificacao)
+            funcionario.pontos += 1
+            funcionario.save()
+
+            return HttpResponse('Ponto registrado com sucesso!')
+        else:
+            return HttpResponse('Funcionário não reconhecido!')
+    return render(request, 'ponto.html')
+
+
+def cadastrar_funcionario(request):
+    if request.method == 'POST':
+        form = FuncionarioForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            funcionario = form.save()
+            return redirect('funcionarios')
+    else:
+        form = FuncionarioForm()
+
+    return render(request, 'cadastrar.html', {'form': form})
